53cb1edeeaefb225a3233b11aaff51b9318d421d
Sergej Kukharev <sergej.kucharev@gmail.com>
2015-02-04 13:46:57 +0200

    0.0.0:3

--- server.js
+++ server.js
@@ -2,26 +2,24 @@
 
 var express = require('express');
 var server = express();
+var debug = require('debug')('app:server');
 var domain = require('domain');
 var domainServer = domain.create();
 
 server.get('/*', function(req, res, next){
-	var r = {
-		'/b': '/xxx12',
-		'/a': '/zxcvbnm'
-	};
+	debug(req.url);
+	var r = require('redirects.json');
 	if (req.url in r) res.status(301).header({'Location': r[req.url], 'Pragma': 'no-cache'}).end('Moved Permanently');
 	else next();
 });
 
-server.get('/xyz', function(req, res){
-	if (req.url.match(/[\/].+/)) {
-		res.send('1');
-	}
-	next();
+server.get('/public/*', function(req, res){
+	res.header({'Pragma': 'no-cache'});
+	res.send('public');
 });
 
 server.get('/*', function(req, res){
+	res.header({'Pragma': 'no-cache'});
 	res.end('.');
 });
 
